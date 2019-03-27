f5a1800d34849ab72ffd760139bf61dfd438df47
John <tingjun.li@qq.com>
2019-03-27 13:28:07 +0800

    crossdomain

--- ynca/face_recognition_ynca.py
+++ ynca/face_recognition_ynca.py
@@ -22,7 +22,10 @@
 # $ pip3 install flask
 
 import face_recognition
-from flask import Flask, jsonify, request, redirect, abort
+from flask import Flask, jsonify, request, redirect, abort, make_response, current_app
+
+from datetime import timedelta
+from functools import update_wrapper
 
 try:
     from flask_cors import CORS, cross_origin  # The typical way to import flask-cors
@@ -45,6 +48,46 @@ from enum import Enum, unique
 import pickle
 import numpy as np
 
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 
 @unique
@@ -153,6 +196,8 @@ def initFacesFromDatafile():
 
 
 def create_app():
+    print "Testing Cors ..."
+
     # initFaces()
     initFacesFromDatafile()    
     app = Flask(__name__)
@@ -331,8 +376,8 @@ def matchN():
 
 
 
-@app.route('/face/image/match', methods=['POST'])
-@cross_origin() 
+@app.route('/face/image/match', methods=['POST',  'OPTIONS'])
+@crossdomain(origin='*')
 def match():
 
     ######################################## check ######################################## 
